7819051f973df9c55b724742582a019aa4b92fbe
sumitgit3 <sumitkumar703011@gmail.com>
2024-08-08 21:25:01 +0530

    added route to get product data from server,from dummy data

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { connect } from 'mongoose';
 import dotenv from 'dotenv'
-
 dotenv.config();
-//create express server
+import products from './DummyProduct.js';
+
+//create express server instance
 const app = express();
 const port = process.env.PORT;
 
@@ -14,6 +15,15 @@ connect(process.env.MONGO_URI)
 
 //middleware->It reads the JSON payload from the request body, converts it into a JavaScript object, and attaches this object to the req.body property.
 app.use(express.json());
+
+//routes
+app.get('/products',(req,res)=>{
+    res.json(products);
+})
+app.get('/products/:id',(req,res)=>{
+    const product = products.find((product)=>product._id === req.params.id);
+    res.json(product);
+})
 
 //server listening
 app.listen(port,()=>{
