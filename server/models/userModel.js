a4f11901c1673f88dc80c55b9a2228df4446a971
sumitgit3 <sumitkumar703011@gmail.com>
2024-08-14 02:44:56 +0530

    Comment:userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -13,11 +13,12 @@ userSchema.methods.matchPassword = async function(enteredPassword){
 }
 
 //function run before saving into data base
-// Encrypt password using bcrypt
+// Encrypt password using bcrypt ->will run when user is created or password is changed.(when user is created password is considered to be changed so it runs)
 userSchema.pre('save', async function (next) {
     // Check if the password field has been modified
+    //this is the document that is about to be saved.
     if (!this.isModified('password')) {
-      return next();
+      return next(); //next is the mongoose next middleware calling ->save()
     }
     // Generate a salt
     const salt = await bcrypt.genSalt(10);
