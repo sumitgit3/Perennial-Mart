83165055534c03b7337adb431ca2179b9f09e882
sumitgit3 <sumitkumar703011@gmail.com>
2024-08-16 23:36:46 +0530

    Fixed alignment of heading in homescreen

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,21 @@ import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/ReactToastify.css'
 
 function App() {
   return (
     <>
       <Header />
-      <main className="text-center py-3">
+      <main className="py-3">
         <Container>
           {/* Outlet component renders the matched child route */}
             <Outlet/>
         </Container>
       </main>
       <Footer />
+      <ToastContainer/>
     </>
 
   );
