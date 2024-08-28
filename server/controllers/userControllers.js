2ae427cbe69fede2018546ee237cbc52535da17e
sumitgit3 <sumitkumar703011@gmail.com>
2024-08-28 23:12:28 +0530

    Fixed bug :was updating admin instead of user

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -162,7 +162,7 @@ const deleteUser = asyncHandler(async (req, res) => {
 //@route PUT /api/users/:id
 //access : PRIVATE/admin
 const updateUser = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id);
+    const user = await User.findById(req.params.id);
     if(user){
         user.name = req.body.name || user.name;
         user.email = req.body.email || user.email;
