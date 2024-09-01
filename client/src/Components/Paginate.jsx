a75917f750601ade1d9661abcfa110e0885eabc2
sumitgit3 <sumitkumar703011@gmail.com>
2024-09-01 23:14:32 +0530

    Pagination component

---
+++ client/src/Components/Paginate.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Pagination } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+const Paginate = ({pageNumber,totalNumberOfPages,isAdmin}) => {
+  return (
+    totalNumberOfPages > 1 && (
+        <Pagination>
+          {
+             [...Array(totalNumberOfPages).keys()].map((x)=>(
+                <LinkContainer 
+                key={x+1}
+                to={isAdmin? `/admin/productList/${x+1}` :`/page/${x+1}`}>
+                    <Pagination.Item active={x+1 === pageNumber}>{x+1}</Pagination.Item>
+                </LinkContainer>
+             ))
+          }
+        </Pagination>
+    )
+  )
+}
+
+export default Paginate
