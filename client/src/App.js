e719e863974eb39da8d3e446f9254f07a54e667f
sumitgit3 <sumitkumar703011@gmail.com>
2024-09-06 21:34:32 +0530

    Aligned footer to bottom of page

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,23 @@ import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import {ToastContainer} from 'react-toastify'
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/ReactToastify.css'
 
 function App() {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
+      {/* min-vh-100 to make the height of container always 100% and flex-grow-1 main to make it take the space */}
       <Header />
-      <main className="py-3">
+      <main className="py-3 flex-grow-1">
         <Container>
           {/* Outlet component renders the matched child route */}
-            <Outlet/>
+          <Outlet />
         </Container>
       </main>
       <Footer />
-      <ToastContainer/>
-    </>
-
+      <ToastContainer />
+    </div>
   );
 }
 
